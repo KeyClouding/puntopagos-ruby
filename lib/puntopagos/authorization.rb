4f76ea89170bbdac61fcb275aa50381b309b6590
juannodeb <juannodeb@gmail.com>
2021-03-03 12:28:21 -0500

    test: missed variable

--- lib/puntopagos/authorization.rb
+++ lib/puntopagos/authorization.rb
@@ -17,9 +17,9 @@ module PuntoPagos
     # Returns the signed String.
     def sign(string)
       # encoded_string = if string.length <= 40
-      #                    Base64.encode64(OpenSSL::HMAC.digest('sha1',@@config.puntopagos_secret, string)).chomp
+                        #  Base64.encode64(OpenSSL::HMAC.digest('sha1',@@config.puntopagos_secret, string)).chomp
       #                  else
-                         Base64.encode64(OpenSSL::HMAC.digest('sha512', @@config.puntopagos_secret, string)).chomp
+                         encoded_string = Base64.encode64(OpenSSL::HMAC.digest('sha512', @@config.puntopagos_secret, string)).chomp
       #                  end
 
       # "PP #{@@config.puntopagos_key.gsub('\n', '')}:#{encoded_string.gsub('\n', '')}"
