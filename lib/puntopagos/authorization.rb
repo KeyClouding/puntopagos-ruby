1e3f2763f0c587df880eb93ef9f88397b9c671c9
juannodeb <juannodeb@gmail.com>
2021-03-03 12:22:11 -0500

    test: trying variations of the code

--- lib/puntopagos/authorization.rb
+++ lib/puntopagos/authorization.rb
@@ -17,20 +17,20 @@ module PuntoPagos
     # Returns the signed String.
     def sign(string)
       encoded_string = if string.length <= 40
-                         Base64.encode64(OpenSSL::HMAC.digest('sha1', @@config.puntopagos_secret, string)).chomp
+                         Base64.encode64(OpenSSL::HMAC.digest('sha1',@@config.puntopagos_secret, string)).chomp
                        else
-                         Base64.encode64(OpenSSL::HMAC.digest('sha512', @@config.puntopagos_secret, string)).chomp
+                         Base64.encode64(OpenSSL::HMAC.digest('sha512',@@config.puntopagos_secret, string)).chomp
                        end
 
       # "PP #{@@config.puntopagos_key.gsub('\n', '')}:#{encoded_string.gsub('\n', '')}"
-      "PP " + @@config.puntopagos_key + ":" + encoded_string
+      "PP "+@@config.puntopagos_key+":"+ encoded_string
     end
 
     # Previous version
-    # def sign(string)
-    #   encoded_string = Base64.encode64(OpenSSL::HMAC.digest('sha1', @@config.puntopagos_secret, string)).chomp
-    #   "PP "+@@config.puntopagos_key+":"+ encoded_string
-    # end
+      # def sign(string)
+      #   encoded_string = Base64.encode64(OpenSSL::HMAC.digest('sha1',@@config.puntopagos_secret, string)).chomp
+      #   "PP "+@@config.puntopagos_key+":"+ encoded_string
+      # end
   end
   # rubocop:enable Layout/LineLength
 end
