3d6799a560b12bdd4ff6317a092935b416b99bbd
juannodeb <juannodeb@gmail.com>
2021-03-03 12:33:08 -0500

    test: original validated version of method

--- lib/puntopagos/authorization.rb
+++ lib/puntopagos/authorization.rb
@@ -16,21 +16,14 @@ module PuntoPagos
     # string - The String to be signed
     # Returns the signed String.
     def sign(string)
-      # encoded_string = if string.length <= 40
-                        encoded_string = Base64.encode64(OpenSSL::HMAC.digest('sha1',@@config.puntopagos_secret, string)).chomp
-      #                  else
-                        #  encoded_string = Base64.encode64(OpenSSL::HMAC.digest('sha512', @@config.puntopagos_secret, string)).chomp
-      #                  end
+      encoded_string = if string.length <= 40
+                         Base64.encode64(OpenSSL::HMAC.digest('sha1',@@config.puntopagos_secret, string)).chomp
+                       else
+                         Base64.encode64(OpenSSL::HMAC.digest('sha512', @@config.puntopagos_secret, string)).chomp
+                       end
 
-      # "PP #{@@config.puntopagos_key.gsub('\n', '')}:#{encoded_string.gsub('\n', '')}"
-      "PP "+@@config.puntopagos_key+":"+ encoded_string
+      "PP " + @@config.puntopagos_key + ":" + encoded_string
     end
-
-    # Previous version
-      # def sign(string)
-      #   encoded_string = Base64.encode64(OpenSSL::HMAC.digest('sha1',@@config.puntopagos_secret, string)).chomp
-      #   "PP "+@@config.puntopagos_key+":"+ encoded_string
-      # end
   end
   # rubocop:enable Layout/LineLength
 end
