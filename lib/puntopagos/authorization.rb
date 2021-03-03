509428066ff7c9ae4fefbb7d58d5472559e638f0
juannodeb <juannodeb@gmail.com>
2021-03-03 15:27:45 -0500

    test: trying strip

--- lib/puntopagos/authorization.rb
+++ lib/puntopagos/authorization.rb
@@ -19,7 +19,7 @@ module PuntoPagos
       # encoded_string = if string.length <= 40
       #                    Base64.strict_encode64(OpenSSL::HMAC.digest('sha1', @@config.puntopagos_secret, string))
       #                  else
-                         encoded_string = Base64.urlsafe_encode64(OpenSSL::HMAC.digest('sha256', @@config.puntopagos_secret, string), padding: false)
+                         encoded_string = Base64.urlsafe_encode64(OpenSSL::HMAC.digest('sha256', @@config.puntopagos_secret, string), padding: false).strip
                       #  end
 
       "PP " + @@config.puntopagos_key + ":" + encoded_string
