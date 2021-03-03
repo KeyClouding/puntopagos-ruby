0eec2d7c602b4547f8f6e54e1567df4d0d057d10
juannodeb <juannodeb@gmail.com>
2021-03-03 11:48:30 -0500

    fix: put encode in one line to check if that solves CRLF error

--- lib/puntopagos/authorization.rb
+++ lib/puntopagos/authorization.rb
@@ -2,6 +2,8 @@ require 'base64'
 require 'openssl'
 
 module PuntoPagos
+  # rubocop:disable Layout/LineLength
+
   # Public: This class manage the signing of a message using
   # the secret and api-key defined in puntopagos.yml
   class Authorization
@@ -15,16 +17,13 @@ module PuntoPagos
     # Returns the signed String.
     def sign(string)
       encoded_string = if string.length <= 40
-                         Base64.encode64(
-                           OpenSSL::HMAC.digest('sha1', @@config.puntopagos_secret, string)
-                         ).chomp
+                         Base64.encode64(OpenSSL::HMAC.digest('sha1', @@config.puntopagos_secret, string)).chomp
                        else
-                         Base64.encode64(
-                           OpenSSL::HMAC.digest('sha512', @@config.puntopagos_secret, string)
-                         ).chomp
+                         Base64.encode64(OpenSSL::HMAC.digest('sha512', @@config.puntopagos_secret, string)).chomp
                        end
 
       "PP #{@@config.puntopagos_key}:#{encoded_string}"
     end
   end
+  # rubocop:enable Layout/LineLength
 end
