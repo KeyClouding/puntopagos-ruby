c5dcc0b20a2d9cc903e57ef6f0b4b6565ed96d32
juannodeb <juannodeb@gmail.com>
2021-03-03 12:24:25 -0500

    test: Only sha512

--- lib/puntopagos/authorization.rb
+++ lib/puntopagos/authorization.rb
@@ -16,11 +16,11 @@ module PuntoPagos
     # string - The String to be signed
     # Returns the signed String.
     def sign(string)
-      encoded_string = if string.length <= 40
-                         Base64.encode64(OpenSSL::HMAC.digest('sha1',@@config.puntopagos_secret, string)).chomp
-                       else
-                         Base64.encode64(OpenSSL::HMAC.digest('sha512',@@config.puntopagos_secret, string)).chomp
-                       end
+      # encoded_string = if string.length <= 40
+      #                    Base64.encode64(OpenSSL::HMAC.digest('sha1',@@config.puntopagos_secret, string)).chomp
+      #                  else
+                         Base64.encode64(OpenSSL::HMAC.digest('sha512', @@config.puntopagos_secret, string)).chomp
+      #                  end
 
       # "PP #{@@config.puntopagos_key.gsub('\n', '')}:#{encoded_string.gsub('\n', '')}"
       "PP "+@@config.puntopagos_key+":"+ encoded_string
