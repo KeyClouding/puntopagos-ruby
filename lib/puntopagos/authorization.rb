112cfe01a10c7810812e9721246075be23ca1dc3
juannodeb <juannodeb@gmail.com>
2021-03-03 15:51:47 -0500

    test: original sha1

--- lib/puntopagos/authorization.rb
+++ lib/puntopagos/authorization.rb
@@ -19,12 +19,20 @@ module PuntoPagos
       # encoded_string = if string.length <= 40
       #                    Base64.strict_encode64(OpenSSL::HMAC.digest('sha1', @@config.puntopagos_secret, string))
       #                  else
-      secret = @@config.puntopagos_secret.gsub!(/[\r\n]+/m, "")
-      secret2 = secret.strip!
-      secret3 = secret.gsub!(" ", "") 
-                         instance = OpenSSL::HMAC.new(secret3, OpenSSL::Digest.new('sha256').squish).squish
-                         encoded_string = Base64.strict_encode64(instance.digest.squish).squish
+
+
+      # secret = @@config.puntopagos_secret.gsub!(/[\r\n]+/m, "")
+      # secret2 = secret.strip!
+      # secret3 = secret.gsub!(" ", "") 
+      #                    instance = OpenSSL::HMAC.new(secret3, OpenSSL::Digest.new('sha256').squish).squish
+      #                    encoded_string = Base64.strict_encode64(instance.digest.squish).squish
+
+
                       #  end
+
+
+      encoded_string = Base64.encode64(OpenSSL::HMAC.digest('sha1', @@config.puntopagos_secret, string)).chomp
+
 
       "PP " + @@config.puntopagos_key + ":" + encoded_string
     end
