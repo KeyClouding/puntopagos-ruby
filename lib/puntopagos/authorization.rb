935107fbe7be27fc3ae736b0cd29e3ce27bb49ab
juannodeb <juannodeb@gmail.com>
2021-03-04 09:21:56 -0500

    test: linting with 1 512 conditional

--- lib/puntopagos/authorization.rb
+++ lib/puntopagos/authorization.rb
@@ -2,11 +2,10 @@ require 'base64'
 require 'openssl'
 
 module PuntoPagos
-  
   # Public: This class manage the signing of a message using
   # the secret and api-key defined in puntopagos.yml
   class Authorization
-    def initialize env = nil
+    def initialize(env = nil)
       @@config ||= PuntoPagos::Config.new(env)
     end
 
@@ -15,8 +14,17 @@ module PuntoPagos
     # string - The String to be signed
     # Returns the signed String.
     def sign(string)
-      encoded_string = Base64.encode64(OpenSSL::HMAC.digest('sha1',@@config.puntopagos_secret, string)).chomp
-      "PP "+@@config.puntopagos_key+":"+ encoded_string
+      encoded_string = if string.length <= 40
+                         Base64.encode64(
+                           OpenSSL::HMAC.digest('sha1', @@config.puntopagos_secret, string)
+                         ).chomp
+                       else
+                         Base64.encode64(
+                           OpenSSL::HMAC.digest('sha512', @@config.puntopagos_secret, string)
+                         ).chomp
+                       end
+
+      "PP #{@@config.puntopagos_key}:#{encoded_string}"
     end
   end
 end
