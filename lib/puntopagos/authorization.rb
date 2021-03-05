8d40f3e685a846dbad67ccbaa00a287b252eee41
juannodeb <juannodeb@gmail.com>
2021-03-05 10:08:35 -0500

    test: sha512 conditional

--- lib/puntopagos/authorization.rb
+++ lib/puntopagos/authorization.rb
@@ -15,7 +15,11 @@ module PuntoPagos
     # string - The String to be signed
     # Returns the signed String.
     def sign(string)
-      encoded_string = Base64.encode64(OpenSSL::HMAC.digest('sha1',@@config.puntopagos_secret, string)).chomp
+      encoded_string = if string.length <= 40
+                         Base64.encode64(OpenSSL::HMAC.digest('sha1',@@config.puntopagos_secret, string)).chomp
+                       else
+                         Base64.encode64(OpenSSL::HMAC.digest('sha512',@@config.puntopagos_secret, string)).chomp
+                       end
       "PP "+@@config.puntopagos_key+":"+ encoded_string
     end
   end
