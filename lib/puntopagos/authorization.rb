e433ca169ab6ef21326448d187f7d18530218add
juannodeb <juannodeb@gmail.com>
2021-03-05 10:11:05 -0500

    test: using strict_encode

--- lib/puntopagos/authorization.rb
+++ lib/puntopagos/authorization.rb
@@ -16,9 +16,9 @@ module PuntoPagos
     # Returns the signed String.
     def sign(string)
       encoded_string = if string.length <= 40
-                         Base64.encode64(OpenSSL::HMAC.digest('sha1',@@config.puntopagos_secret, string)).chomp
+                         Base64.strict_encode64(OpenSSL::HMAC.digest('sha1',@@config.puntopagos_secret, string)).chomp
                        else
-                         Base64.encode64(OpenSSL::HMAC.digest('sha512',@@config.puntopagos_secret, string)).chomp
+                         Base64.strict_encode64(OpenSSL::HMAC.digest('sha512',@@config.puntopagos_secret, string)).chomp
                        end
       "PP "+@@config.puntopagos_key+":"+ encoded_string
     end
