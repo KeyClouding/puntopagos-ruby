563cbe0a215fb9cf36256492a8ccc46a6001a817
juannodeb <juannodeb@gmail.com>
2021-03-17 12:40:34 -0500

    test: Added puts in sign method

--- lib/puntopagos/authorization.rb
+++ lib/puntopagos/authorization.rb
@@ -16,11 +16,15 @@ module PuntoPagos
     # string - The String to be signed
     # Returns the signed String.
     def sign(string)
+      puts "PPRUBY RESPONSE Authorization::sign =====> #{string.inspect}"
       encoded_string = if string.length <= 40
                          Base64.strict_encode64(OpenSSL::HMAC.digest('sha1',@@config.puntopagos_secret, string)).chomp
                        else
                          Base64.strict_encode64(OpenSSL::HMAC.digest('sha512',@@config.puntopagos_secret, string)).chomp
                        end
+      puts "PPRUBY RESPONSE Authorization::sign 1 =====> #{encoded_string.inspect}"
+      result = "PP "+@@config.puntopagos_key+":"+ encoded_string
+      puts "PPRUBY RESPONSE Authorization::sign 2 =====> #{result.inspect}"
       "PP "+@@config.puntopagos_key+":"+ encoded_string
     end
   end
